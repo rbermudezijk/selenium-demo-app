317735ac7260852c6a1530287a277ce6f9546985
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2024-11-26 14:35:52 -0600

    First commit

---
+++ src/test/java/com/example/selenium/selenium_demo_app/SeleniumDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selenium.selenium_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
